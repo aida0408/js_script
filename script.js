d3c8ef51d0058f9b54c04d0d788d521e90a80db5
Bakazova Aida <solnyshko669@gmail.com>
2022-01-29 23:57:16 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// const name = 'Dima'
+// if(name === 'Roma') {
+//     console.log('Privet, Roma!')
+// }else {
+//     console.log('Ya tebya ne znayu!')
+// }
+
+// const name = "John"
+// if (name === "Roma"){
+//     console.log("Privet, Roma!")
+// }else if (name ==="Dima"){
+//     console.log("Privet, Dima!")
+// }else if (name === "Katya"){
+//     console.log("Privet, Katya!")
+// }else{
+//     console.log("Ya tebya ne znayu!")
+// }
+
