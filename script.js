c3ea0cc9b33e2f0cf809325bd1116b61552b4f51
Bakazova Aida <solnyshko669@gmail.com>
2022-01-30 00:52:03 +0600

    task about string

--- script.js
+++ script.js
@@ -23,15 +23,31 @@
 // }else{
 //     console.log("zero")
 // }
-const time = 105
-if (time > 0 && time <= 15){
-    console.log('Первая четверть')
-}else if (time >= 16 && time <= 30){
-    console.log('Вторая четверть')
-}else if (time >= 31 && time <= 45){
-    console.log('Третья четверть')
-}else if (time >= 46 && time <= 60){
-    console.log('Четвертая четверть')
-}else {
-    console.log('Такой минуты нет')
-}
+// const time = 105
+// if (time > 0 && time <= 15){
+//     console.log('Первая четверть')
+// }else if (time >= 16 && time <= 30){
+//     console.log('Вторая четверть')
+// }else if (time >= 31 && time <= 45){
+//     console.log('Третья четверть')
+// }else if (time >= 46 && time <= 60){
+//     console.log('Четвертая четверть')
+// }else {
+//     console.log('Такой минуты нет')
+// }
+
+// const num = 10
+// if ( num % 2 === 0){
+//     console.log("chetnoe")
+// }else {
+//     console.log('nechetnoe')
+// }
+
+// let data = 'jslsls'
+// if (typeof data === 'string'){
+//     console.log("Строка")
+// }else if ( typeof data === 'number'){
+//     console.log('Число')
+// }else if ( typeof data === 'boolean'){
+//     console.log('Булеан')
+// }
